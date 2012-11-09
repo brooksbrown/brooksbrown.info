fe9154eecfc1049be9bd8ca5ac60e0ab3c0e51de
davibrosk <davibrosk@deckard.(none)>
2012-11-08 20:54:01 -0500

    initial setup

--- cms/blog/models.py
+++ cms/blog/models.py
@@ -2,13 +2,32 @@ from django.db import models
 from django.contrib import admin
 
 # Create your models here.
+
+
+class ArticleTerm(models.Model):
+  title = models.CharField(max_length=100)
+
+  def __unicode__(self):
+    return self.title
+
+
+
+
 class Article(models.Model):
   title = models.CharField(max_length=200)
   pub_date = models.DateTimeField('date published')
   body = models.TextField()
+  terms = models.ManyToManyField(ArticleTerm)
+  def __unicode__(self):
+    return self.title
 
+
+
+#admin calls
 class ArticleAdmin(admin.ModelAdmin):
       pass
+class ArticleTermAdmin(admin.ModelAdmin):
+    pass
 
-
+admin.site.register(ArticleTerm, ArticleTermAdmin)
 admin.site.register(Article, ArticleAdmin)
--- cms/urls.py
+++ cms/urls.py
@@ -16,7 +16,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^grappelli/', include('grappelli.urls')),
-    url(r'^blog/$', 'blog.views.index'),
+    url(r'^$', 'blog.views.index'),
 )
 
 
