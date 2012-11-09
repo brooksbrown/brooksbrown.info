5a8d97e9100d71e7c640e84f30791cbbee8dc28d
brooksbrown <brooks@li425-113.members.linode.com>
2012-11-09 18:20:32 +0000

    removed db settings from settings file

--- cms/settings.py
+++ cms/settings.py
@@ -1,5 +1,6 @@
 # Django settings for cms project.
 import os
+import MySQLdb
 
 os.environ['DJANGO_SETTINGS_MODULE'] = "cms.settings"
 DEBUG = True
@@ -11,17 +12,17 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'test.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
-
+from databasesettings import DATABASES
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+#        'NAME': '',                      # Or path to database file if using sqlite3.
+#        'USER': '',                      # Not used with sqlite3.
+#        'PASSWORD': '',                  # Not used with sqlite3.
+#        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+#        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+#    }
+#}
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
@@ -74,7 +75,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/vagrant/cms/static",
+    "/var/www/brooksbrown.info/cms/static",
 )
 
 # List of finder classes that know how to find static files in
@@ -109,7 +110,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "templates",
+    "/var/www/brooksbrown.info/cms/templates",
 )
 
 INSTALLED_APPS = (
@@ -150,3 +151,4 @@ LOGGING = {
         },
     }
 }
+
